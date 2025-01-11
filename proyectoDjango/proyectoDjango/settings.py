ac85897a9caed53f4ac3431465363cd546092619
Franvalenz <franvalenz77@gmail.com>
2025-01-11 15:12:21 -0300

    se agrega 'grappelli en settings.py'

--- proyectoDjango/proyectoDjango/settings.py
+++ proyectoDjango/proyectoDjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'grappelli', # Se añade la app de grappelli
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
