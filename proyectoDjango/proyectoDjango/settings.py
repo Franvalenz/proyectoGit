7ec51a55d85a045b13b3ddb997aa252a890d0005
Franvalenz <franvalenz77@gmail.com>
2025-01-10 23:58:42 -0300

    se agrego miAppDjango a setting.py

--- proyectoDjango/proyectoDjango/settings.py
+++ proyectoDjango/proyectoDjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'miAppDjango', # Se añade la app creada
 ]
 
 MIDDLEWARE = [
