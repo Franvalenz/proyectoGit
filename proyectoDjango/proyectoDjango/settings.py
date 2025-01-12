9c7683dc9019c02caf3cb694e3db99e6e78d20cc
Franvalenz <franvalenz77@gmail.com>
2025-01-11 21:29:20 -0300

    se añadio css y ruta settings.py

--- proyectoDjango/proyectoDjango/settings.py
+++ proyectoDjango/proyectoDjango/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'proyectoDjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Se añade la ruta de la carpeta templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static", # Se añade la ruta de la carpeta static
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
